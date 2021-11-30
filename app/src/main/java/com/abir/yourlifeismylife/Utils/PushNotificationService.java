cfdf8572de2f1d91a966a8c15317c880001e6957
MeshMeshNader <meshmeshnader555@gmail.com>
2021-11-30 15:32:48 +0200

    Add push noti

---
+++ app/src/main/java/com/abir/yourlifeismylife/Utils/PushNotificationService.java
@@ -0,0 +1,38 @@
+package com.abir.yourlifeismylife.Utils;
+
+import android.app.Notification;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.os.Build;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.RequiresApi;
+import androidx.core.app.NotificationCompat;
+import androidx.core.app.NotificationManagerCompat;
+
+import com.abir.yourlifeismylife.R;
+import com.google.firebase.messaging.FirebaseMessagingService;
+import com.google.firebase.messaging.RemoteMessage;
+
+public class PushNotificationService extends FirebaseMessagingService {
+    @RequiresApi(api = Build.VERSION_CODES.O)
+    @Override
+    public void onMessageReceived(@NonNull RemoteMessage remoteMessage) {
+        String title  = remoteMessage.getNotification().getTitle();
+        String text  = remoteMessage.getNotification().getBody();
+        final String CHANNEL_ID = "HEADS_UP_NOTIFICATION";
+        NotificationChannel channel = new NotificationChannel(
+                CHANNEL_ID,
+                "Heads Up Notification",
+                NotificationManager.IMPORTANCE_HIGH
+        );
+        getSystemService(NotificationManager.class).createNotificationChannel(channel);
+        Notification.Builder notification = new Notification.Builder(this, CHANNEL_ID)
+                .setContentTitle(title)
+                .setContentText(text)
+                .setSmallIcon(R.drawable.ic_baseline_family_restroom_24)
+                .setAutoCancel(true);
+        NotificationManagerCompat.from(this).notify(1 , notification.build());
+        super.onMessageReceived(remoteMessage);
+    }
+}
